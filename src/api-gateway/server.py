6efe89e80bf9982e4986f1f112b7457ae10b82f0
Anna Lobach <119875559+alobach3@users.noreply.github.com>
2026-03-27 23:42:11 +0100

    test commit

--- src/api-gateway/server.py
+++ src/api-gateway/server.py
@@ -45,11 +45,11 @@ def pay():
     res = payment_client.Charge(demo_pb2.PaymentRequest(amount=data["amount"]))
     return jsonify({"status": res.status})
 
-# test comments
 #if __name__ == "__main__":
 #    print("API Gateway running on http://localhost:8080")
 #    app.run(port=8080)
 
+# test comment
 if __name__ == "__main__":
     print("API Gateway running on http://0.0.0.0:8080")
     app.run(host="0.0.0.0", port=8080)
