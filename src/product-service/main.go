f5269dbaec6fee4cd284d01cc37ecb7d349748bb
Anna Lobach <119875559+alobach3@users.noreply.github.com>
2026-03-27 22:16:27 +0100

    Adjust product prices in ListProducts method
    Updated product prices for better accuracy.

--- src/product-service/main.go
+++ src/product-service/main.go
@@ -17,11 +17,11 @@ type server struct {
 func (s *server) ListProducts(ctx context.Context, req *pb.Empty) (*pb.ProductList, error) {
 
 	products := []*pb.Product{
-		{Id: 1, Name: "Anime Figure 1/6", Category: "figure", Price: 219, Image: "https://via.placeholder.com/400x400"},
-		{Id: 2, Name: "Anime Figure 1/7", Category: "figure", Price: 69, Image: "https://via.placeholder.com/400x400"},
-		{Id: 3, Name: "Anime Keycap", Category: "keyboard", Price: 69, Image: "https://via.placeholder.com/400x400"},
-		{Id: 4, Name: "Anime Accessories", Category: "accessories", Price: 123, Image: "https://via.placeholder.com/400x400"},
-		{Id: 5, Name: "Abune Bundle", Category: "bundle", Price: 384, Image: "https://via.placeholder.com/400x400"},
+		{Id: 1, Name: "Anime Figure 1/6", Category: "figure", Price: 200, Image: "https://via.placeholder.com/400x400"},
+		{Id: 2, Name: "Anime Figure 1/7", Category: "figure", Price: 67, Image: "https://via.placeholder.com/400x400"},
+		{Id: 3, Name: "Anime Keycap", Category: "keyboard", Price: 67, Image: "https://via.placeholder.com/400x400"},
+		{Id: 4, Name: "Anime Accessories", Category: "accessories", Price: 100, Image: "https://via.placeholder.com/400x400"},
+		{Id: 5, Name: "Abune Bundle", Category: "bundle", Price: 300, Image: "https://via.placeholder.com/400x400"},
 	}
 
 	return &pb.ProductList{Products: products}, nil
